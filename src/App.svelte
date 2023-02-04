7a9578c707aef040002733838c39bcd076d2dae0
malawadd <100777093+0xgoldenlion@users.noreply.github.com>
2023-02-04 21:27:41 +0300

    adjust version and theme

--- src/App.svelte
+++ src/App.svelte
@@ -1,212 +1,111 @@
 <script>
-  import { onMount } from "svelte";
 
-  import Modals from "@components/layout/Modals.svelte";
-  import Errors from "@components/layout/Errors.svelte";
-  import Toasts from "@components/layout/Toasts.svelte";
-  import Header from "@components/header/Header.svelte";
+	import { onMount } from 'svelte'
 
-  import { loadRoute, catchLinks, navigateTo } from "@lib/routing";
-  import { component, address, pageName } from "@lib/stores";
-  import { hidePopoversOnKeydown, hidePopoversOnClick } from "@lib/ui";
-  import { runAndInterval } from "@lib/utils";
+	import Modals from './components/layout/Modals.svelte'
+	import Toasts from './components/layout/Toasts.svelte'
+	import Header from './components/layout/Header.svelte'
 
-  import { listenToEvents } from "@api/listener";
-  import { getMarketsWithPrices } from "@api/markets";
+	import { monitorOracleResponse } from './lib/monitor'
+	import { initWebsocket } from './lib/stream'
+	import { component } from './lib/stores'
+	import { loadRoute, navigateTo, catchLinks, hidePopoversOnClick } from './lib/utils'
 
-  onMount(async () => {
-    loadRoute();
-    catchLinks((path) => navigateTo(path));
-    window.onpopstate = () => loadRoute();
-    runAndInterval(getMarketsWithPrices, 10 * 1000);
-  });
+	// localstorage clearance for v2
+	if (localStorage.getItem('productId')*1 > 0) {
+		localStorage.removeItem('productId');
+		localStorage.removeItem('leverage');
+		localStorage.removeItem('cachedLeverages');
+	}
 
-  // Listener
-  $: listenToEvents($address);
+	onMount(async () => {
+		loadRoute(location.hash, true);
+		catchLinks((path) => navigateTo(path));
+		hidePopoversOnClick();
+
+		// For back button functionality
+		window.onpopstate = () => loadRoute(location.hash);
+
+		initWebsocket();
+		monitorOracleResponse();
+	});
+
 </script>
 
-<svelte:window on:keydown={hidePopoversOnKeydown} on:click={hidePopoversOnClick} />
-
-{#if $pageName != "Home"}
-  <Errors />
-  <Modals />
-  <Toasts />
-  <Header />
-{/if}
-
-<svelte:component this={$component} />
-
 <style>
-  :global(:root) {
-    /*Principles:  
-			https://material.io/design/color/dark-theme.html
-			https://developer.apple.com/design/human-interface-guidelines/foundations/color/
-		*/
 
-    color-scheme: dark;
+	/*Global styles*/
 
-    --container-width: 800px;
+	/*Only keep color vars here, rest can go to global.css reset stylesheet*/
 
-    --base-padding: 20px;
-    --base-radius: 6px;
-    --semi-padding: calc(var(--base-padding) / 2);
-    --header-height: 75px;
-    --sidebar-width: 320px;
-    --ticker-height: 60px;
+	:global(:root) {
 
-    /* Layers (gray), from darkest to lightest */
-    --layerDark: rgb(25, 29, 32);
-    --layer0: rgb(29, 34, 38);
-    --layer25: #222322;
-    --layer50: rgb(36, 41, 46);
-    --layer100: #333433;
-    --layer200: #494a49;
-    --layer300: #606160;
-    --layer400: #777777;
-    --layer500: #8e8e8e;
+		--red: #FF5000;
+		--red-dim: #E04700;
+		--red-dark: #421500;
+		--green: #00D604;
+		--green-dim: #00C403;
+		--green-dark: #004D01;
 
-    /* Text (white), from lightest to darkest */
-    --text0: #ffffff;
-    --text100: #e6e6e6;
-    --text200: #cccccc;
-    --text300: #b3b3b3;
-    --text400: #999999;
-    --text500: #808080;
+		--rich-black: #080808;
+		--rich-black-fogra: #0F0F0F;
+		--eerie-black: #1A1A1A;
+		--jet-dim: #212121;
+		--jet: #292929;
+		--onyx-dim: #303030;
+		--onyx: #3D3D3D;
+		--dim-gray: #4A4A4A;
+		--sonic-silver: #707070;
+		--silver-chalice:  #ADADAD;
+		--orange: rgb(253,167,20);
+		
+		--base-padding: 16px;
+		--semi-padding: 8px;
+		--base-radius: 4px;
 
-    /* Brand */
+		--chart-resolution-height: 40px;
+		--chart-height: 430px;
+		--header-height: 60px;
+		--ticker-height: 60px;
+		--grid-gap: 1px;
 
-    --primary: rgba(50, 209, 53, 1);
-    --primary-highlighted: rgba(50, 209, 53, 0.1);
-    --primary-active: rgba(50, 209, 53, 0.75);
-    --primary-hover: rgba(50, 209, 53, 0.94);
-    --primary-darkest: #121212;
+	}
 
-    --secondary: rgba(248, 76, 32, 1);
-    --secondary-highlighted: rgba(248, 76, 32, 0.1);
-    --secondary-active: rgba(248, 76, 32, 0.72);
-    --secondary-hover: rgba(248, 76, 32, 0.91);
-    --secondary-darkest: #121212;
+	@media (max-height: 800px) {
+	  :global(:root) {
+	    --chart-height: 380px;
+	  }
+	}
 
-    --error: orange;
-    --error-lighter: #ffc099;
-    --error-light: #ff9e61;
-    --error-dark: #291000;
-    --error-border: #ff680a;
+	@media (max-height: 700px) {
+	  :global(:root) {
+	    --chart-height: 320px;
+	  }
+	}
 
-    --accent: blue; /*neutral, can be used for info, etc.*/
+	@media (max-height: 600px) {
+	  :global(:root) {
+	    --chart-height: 240px;
+	  }
+	}
 
-    /* Old below */
+	@media (max-height: 500px) {
+	  :global(:root) {
+	    --chart-height: 180px;
+	  }
+	}
 
-    --layer0dot5: #2b2d2f;
-    --layer0-hover: rgb(36, 38, 40);
-    --layer1: #36383a;
-    --layer1-hover: rgb(52, 52, 58);
+	:global(.pos) {
+		color: var(--green);
+	}
+	:global(.neg) {
+		color: var(--red);
+	}
 
-    --layer1dot5: rgb(48, 48, 50);
-    --layer2: #4d4e50;
-    --layer3: #636466;
-    --layer4: #797a7c;
-    --layer4dot5: rgb(115, 115, 119);
-    --layer5: rgb(142, 142, 147);
-    --layer6: rgb(174, 174, 178);
-    --layer7: rgb(199, 199, 204);
-    --layer8: rgb(209, 209, 214);
-    --layer9: rgb(229, 229, 234);
-
-    /* Texts from lightest to darkest */
-
-    --text0-hover: rgba(255, 255, 255, 0.85);
-    --text1: rgb(182, 182, 187);
-    --text1-hover: rgba(182, 182, 187, 0.85);
-    --text1-alt: rgb(122, 122, 137);
-
-    --text05: rgba(255, 255, 255, 0.85);
-    --text005: rgba(255, 255, 255, 0.65);
-
-    --text2: rgb(90, 90, 94);
-    --text3: rgb(64, 64, 68);
-  }
-
-  :global(html) {
-    color: var(--text0);
-    background-color: var(--layer0);
-  }
-
-  :global(.green) {
-    color: var(--primary);
-  }
-  :global(.red) {
-    color: var(--secondary);
-  }
-  :global(.orange) {
-    color: rgb(253, 167, 20);
-  }
-
-  :global(.container) {
-    padding: var(--base-padding);
-  }
-  :global(.group) {
-    padding-bottom: var(--base-padding);
-  }
-  :global(.semi-padding-bottom) {
-    padding-bottom: var(--semi-padding);
-  }
-
-  /*Tippy Tooltip*/
-  :global(.tippy-box[data-theme~="cap"]) {
-    background-color: var(--layer200);
-    color: var(--text0);
-    font-weight: 400;
-    font-size: 14px;
-  }
-  :global(.tippy-box[data-theme~="cap"][data-placement^="top"] > .tippy-arrow::before) {
-    border-top-color: var(--layer200);
-  }
-  :global(.tippy-box[data-theme~="cap"][data-placement^="bottom"] > .tippy-arrow::before) {
-    border-bottom-color: var(--layer200);
-  }
-  :global(.tippy-box[data-theme~="cap"][data-placement^="left"] > .tippy-arrow::before) {
-    border-left-color: var(--layer200);
-  }
-  :global(.tippy-box[data-theme~="cap"][data-placement^="right"] > .tippy-arrow::before) {
-    border-right-color: var(--layer200);
-  }
-
-  .code-check {
-    padding: 25px;
-  }
-  .note {
-    padding: 25px;
-    max-width: 440px;
-    line-height: 1.318;
-  }
-  input {
-    width: 120px;
-    box-sizing: border-box;
-    margin-bottom: 10px;
-    font-size: inherit;
-    padding: 4px 10px;
-  }
-  button {
-    padding: 4px 10px;
-    width: 120px;
-  }
-  :global(button.primary, button.secondary) {
-    padding: 0 var(--base-padding);
-    height: 42px;
-    border-radius: var(--base-radius);
-    font-weight: 600;
-    color: var(--primary-darkest);
-    background-color: var(--primary);
-    white-space: nowrap;
-    font-size: 95%;
-  }
-  :global(button.secondary) {
-    background-color: var(--secondary);
-    color: var(--secondary-darkest);
-  }
-  :global(.buttons) {
-    display: flex;
-    justify-content: space-around;
-  }
 </style>
+
+<Modals />
+<Toasts />
+
+<Header />
+<svelte:component this={$component}/>
